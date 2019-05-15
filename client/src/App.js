dae6ed509d846c7c3844f28b3cd5bdbbdbe61927
ruvimsypa <ruvimsypa@gmail.com>
2019-05-15 18:21:39 +0300

    Create React App

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Landing from "./components/layuot/Landing";
+import Navbar from "./components/layuot/Navbar";
+
+const App = () => (
+  <Fragment>
+    <Navbar/>
+    <Landing/>
+  </Fragment>
+);
+
+export default App;
